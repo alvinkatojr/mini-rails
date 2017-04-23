6c8b831f4d90d9d4e61cb14086391b7a21d1f1cd
alvinkatojr <alvinkatojr@gmail.com>
2017-04-24 02:01:01 +0300

    Adds execute method

--- lib/active_record/connection_adapter.rb
+++ lib/active_record/connection_adapter.rb
@@ -5,6 +5,19 @@ module ActiveRecord
         require "sqlite3"
         @db = SQLite3::Database.new(file.to_s, results_as_hash: true)
       end
+
+      # Executes a SQL query and returns result as an array of hashes
+      def execute(sql)
+        @db.execute(sql).each do |row|
+          row.keys.each do |key|
+            value = row.delete(key)
+            # Only keep strings keys (ignores index-based key, 0, 1, ...)
+            if key.is_a? String
+              row[key.to_sym] = value
+            end
+          end
+        end
+      end
     end
   end
 end
