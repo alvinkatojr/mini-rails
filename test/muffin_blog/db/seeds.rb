eee9fd01e6384bd5e9ef6d04222f923788051d3b
alvinkatojr <alvinkatojr@gmail.com>
2017-04-24 02:30:12 +0300

    Some seeds, seeds, seeds!

--- test/muffin_blog/db/seeds.rb
+++ test/muffin_blog/db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create!([{ title: 'Star Wars' }, {body: 'Lord of the Rings was Lord of the box office over the weekend....' }])
+Post.create!(title: 'Ogiers wins 5th WRC title!', body: 'The perfectionist that is Ogier dominated Rally Australia to bring what had been a thrilling 2 way fight for the title to an end. Ogier hampered by....')
