22edf9aa1bc54bd49855ad7c27f92fa95a81ec8e
alvinkatojr <alvinkatojr@gmail.com>
2017-04-24 03:55:08 +0300

    Booboo, typos and goof ups with the seeds

--- test/muffin_blog/db/seeds.rb
+++ test/muffin_blog/db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 
-Post.create!([{ title: 'Star Wars' }, {body: 'Lord of the Rings was Lord of the box office over the weekend....' }])
+Post.create!(title: 'Star Wars', body: 'Lord of the Rings was Lord of the box office over the weekend....')
 Post.create!(title: 'Ogiers wins 5th WRC title!', body: 'The perfectionist that is Ogier dominated Rally Australia to bring what had been a thrilling 2 way fight for the title to an end. Ogier hampered by....')
