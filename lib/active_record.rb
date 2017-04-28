b59a5b7c983c08c2900777d575e996dee68abc43
alvinkatojr <alvinkatojr@gmail.com>
2017-04-28 23:23:29 +0300

    Replaces posts with generic table_name method call

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -7,7 +7,7 @@ module ActiveRecord
     end
 
     def method_missing(name, *args)
-      columns = self.class.connection.columns("posts")
+      columns = self.class.connection.columns("#{self.class.table_name}")
 
       if columns.include?(name)
         @attributes[name]
@@ -24,12 +24,12 @@ module ActiveRecord
     end
 
     def self.find(id)
-      attributes = connection.execute("SELECT * from posts WHERE id = #{id.to_i}").first
+      attributes = connection.execute("SELECT * from #{table_name} WHERE id = #{id.to_i}").first
       new(attributes)
     end
 
     def self.all
-      connection.execute("SELECT * from posts").map do |attributes|
+      connection.execute("SELECT * from #{table_name}").map do |attributes|
         new(attributes)
       end
     end
