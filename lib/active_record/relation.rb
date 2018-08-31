7b48e291b430e4fa73ff3202433e8bff0520c6cf
alvinkatojr <alvinkatojr@gmail.com>
2018-08-31 03:58:49 +0300

    Add initialize method that stores where value conditions and bang where method that modifies the relation object

--- lib/active_record/relation.rb
+++ lib/active_record/relation.rb
@@ -1,4 +1,12 @@
 module ActiveRecord
   class Relation
+    def initialize
+      where_values = []
+    end
+
+    def where!(condition)
+      where_values += [condition]
+      self
+    end
   end
 end
