78ca7b459498a0e09e2f36bb5497e968e59443cc
alvinkatojr <alvinkatojr@gmail.com>
2018-09-27 07:03:37 +0300

    Adds to_sql method, reference to record class in initializer and fixes where_values missing instance variable '@' sign

--- lib/active_record/relation.rb
+++ lib/active_record/relation.rb
@@ -1,16 +1,27 @@
 module ActiveRecord
   class Relation
-    def initialize
-      where_values = []
+    def initialize(klass)
+      @klass = klass
+      @where_values = []
     end
 
     def where!(condition)
-      where_values += [condition]
+      @where_values += [condition]
       self
     end
 
     def where(condition)
       clone.where!(condition)
+    end
+
+    def to_sql
+      sql = "SELECT * FROM #{klass.table_name}"
+
+      if @where_values.any?
+        sql += " WHERE " + @where_values.join(" AND ")
+      end
+
+      sql
     end
   end
 end
