5b89c367e5ef4145c9980486d069aafbaea7174e
alvinkatojr <alvinkatojr@gmail.com>
2018-10-02 21:32:42 +0300

    Initializes all look up paths containing application files

--- test/active_support_test.rb
+++ test/active_support_test.rb
@@ -2,5 +2,7 @@ require 'test_helper'
 require 'active_support'
 
 class ActiveSupportTest < Minitest::Test
-
+  def setup
+    ActiveSupport::Dependencies.autoload_paths = Dir["#{__dir__}/muffin_blog/app/*"]
+  end
 end
