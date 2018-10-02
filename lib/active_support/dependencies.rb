6aa96fdaecfb090f17ac86a8db442d7f319e3900
alvinkatojr <alvinkatojr@gmail.com>
2018-10-02 21:57:56 +0300

    Adds search_for_file method, test passes

--- lib/active_support/dependencies.rb
+++ lib/active_support/dependencies.rb
@@ -4,5 +4,15 @@ module ActiveSupport
 
     attr_accessor :autoload_paths
     self.autoload_paths = []
+
+    def search_for_file(name)
+      autoload_paths.each do |path|
+        file = File.join(path, "#{name}.rb")
+        if File.file? file
+          return file
+        end
+      end
+      nil
+    end
   end
 end 
