3d156abcfa3b017dd7cf5a9ea465f55c94034f9e
alvinkatojr <alvinkatojr@gmail.com>
2019-04-09 17:55:07 +0300

    Activates the rails app root via the config enviroment path using the caller method

--- lib/rails/application.rb
+++ lib/rails/application.rb
@@ -8,5 +8,16 @@ module Rails
     def self.instance
       @instance
     end
+
+    def initialize!
+      config_environment_path = caller.first
+      @root = Pathname.new(File.expand_path("../..", config_environment_path))
+
+      ActiveSupport::Dependencies.autoload_paths = Dir["#{@root}/app/*"]
+    end
+
+    def root
+      @root
+    end
   end
 end
