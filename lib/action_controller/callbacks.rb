46ca5125d1b135b1285bbf0779ed6b05a4c78912
alvinkatojr <alvinkatojr@gmail.com>
2019-04-23 16:42:59 +0300

    Adds before_action class method which stores all callback instances in an array to be called later

--- lib/action_controller/callbacks.rb
+++ lib/action_controller/callbacks.rb
@@ -1,4 +1,17 @@
 module ActionController
   module Callbacks
+    def self.include(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def before_action(method, options={})
+        before_actions << Callback.new(method, options)
+      end
+
+      def before_actions
+        @before_action ||= []
+      end
+    end
   end
 end
