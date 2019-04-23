0d389607d5fd7eb3437cc0976270551cb4bb668e
alvinkatojr <alvinkatojr@gmail.com>
2019-04-23 15:39:50 +0300

    Adds test controller and test assertion for response object

---
+++ test/action_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ActionControllerTest < Minitest::Test
+  class TestController < ActionController::Base
+    def index
+      response << "index"
+    end
+
+    def test_calls_index
+      controller = TestController.new
+      controller.response = []
+      controller.index
+
+      assert_equal ["index"], controller.response
+    end
+  end
+end
